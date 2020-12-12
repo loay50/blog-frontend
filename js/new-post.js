5f96caef464d67f357902361def4a2e964990e85
loay50 <57413061+loay50@users.noreply.github.com>
2020-12-12 22:47:55 +0200

    first commit

---
+++ js/new-post.js
@@ -0,0 +1,28 @@
+/**
+ * TODO: Finish submitNewPost function to submit form data to the API 
+ */
+
+const API_URL = "http://localhost:3000/api/posts";
+
+const submitNewPost = () => {
+  
+    const input = document.getElementById("form-post-image")//.files[0]
+    const title = document.getElementById("form-post-title").value
+    const content = document.getElementById("form-post-content").value
+
+    let data = new FormData()
+    data.append("post-image", input.files[0])
+    data.append("title", title)
+    data.append("content", content)
+
+    fetch(API_URL,{
+        method:"POST",
+        body: data
+    }).then(()=>{
+        setTimeout(()=>{
+
+        
+        window.location.href="index.html"
+        },1000)
+    })
+}
